75705f136aa9ded1a521417a798a90a07bc310ac
MayazinhaIX <hstrom962@gmail.com>
2026-07-29 14:57:32 -0300

    Preparação de ambiente para o uso do Typescript

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+console.log("Olá Typescript!");
+
+console.log("Seja bem-vindo!");
+
+console.log("Como você se sente hoje?");
+
+console.log("Que bom que se sente bem!");
+
+console.log("Fico feliz com o seu bem-estar");
